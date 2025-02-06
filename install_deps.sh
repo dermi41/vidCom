67de84fe87ccbd8d51eff346f797bda5e1baeafc
M <dermottxx@gmail.com>
2025-02-06 11:44:53 +0100

    modified:   install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-# Install FFmpeg
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar xJ
-mv ffmpeg-*-static/ffmpeg ffmpeg-*-static/ffprobe /usr/local/bin/
+
+# Install FFmpeg in a custom directory
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz | tar xJ
+mkdir -p $HOME/bin
+mv ffmpeg-*-static/ffmpeg $HOME/bin/
+mv ffmpeg-*-static/ffprobe $HOME/bin/
+
+# Add the custom directory to the PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
 
 # Install yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
-chmod a+rx /usr/local/bin/yt-dlp
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o $HOME/bin/yt-dlp
+chmod a+rx $HOME/bin/yt-dlp
+
+# Ensure that the custom bin directory is in the PATH
+echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
